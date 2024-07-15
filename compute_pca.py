e469869f747b3c56cdece0c127f681a0a74e2cbf
Michael Ishak <108626872+Michael-Ishak@users.noreply.github.com>
2024-07-15 01:44:52 +0100

    computed pca on RSI

---
+++ compute_pca.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import ta.momentum
+from scipy import linalg
+
+plt.style.use("dark_background")
+
+data = pd.read_csv("XAUUSD.raw_M10_202201030300_202312292350.csv")
+
+rsi_periods = list(range(2, 25))
+
+rsis = pd.DataFrame()
+for period in rsi_periods:
+    rsis[period] = ta.momentum.rsi(data["<CLOSE>"], period, True)
+
+rsi_mu = rsis.mean()
+rsis -= rsi_mu
+rsis = rsis.dropna()
+
+cov = np.cov(rsis, rowvar=False)
+evals, evecs = linalg.eigh(cov)
+
+idx = np.argsort(evals)[::-1]
+evecs = evecs[:, idx]
+evals = evals[idx]
+
+n_component = 4
+rsi_pca = pd.DataFrame()
+for no_comp in range(n_component):
+    rsi_pca["PC" + str(no_comp)] = pd.Series(
+        np.dot(rsis, evecs[no_comp]), index=rsis.index
+    )
+
+for no_comp in range(n_component):
+    pd.Series(evecs[no_comp], index=rsi_periods).plot(label="PC" + str(no_comp + 1))
+
+plt.xlabel("RSI Period")
+plt.ylabel("Eigenvector Value")
+plt.legend()
+plt.show()
